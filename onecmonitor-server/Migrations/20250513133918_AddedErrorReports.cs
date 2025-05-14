f1cea3c4f0f89cfedbc9865bb901c2c536c74f14
akpaev.e <akpaevj@gmail.com>
2025-05-14 09:14:34 +0300

    Промежуточная

---
+++ onecmonitor-server/Migrations/20250513133918_AddedErrorReports.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnecMonitor.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedErrorReports : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Date",
+                table: "ErrorReports",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "ErrorReports");
+        }
+    }
+}
