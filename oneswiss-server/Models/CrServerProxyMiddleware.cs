f3237cfed684f94f42f1bcd9d0619694a443243a
akpaev.e <akpaevj@gmail.com>
2025-09-10 18:10:51 +0300

    Добавлена возможность указания нескольких локаций в обработчике

--- oneswiss-server/Models/CrServerProxyMiddleware.cs
+++ oneswiss-server/Models/CrServerProxyMiddleware.cs
@@ -9,12 +9,9 @@ public class CrServerProxyMiddleware : DatabaseObject
     public bool DebugMode { get; set; }
     public string ExecutablePath { get; set; } = string.Empty;
     public Guid? FileId { get; set; }
-    public Guid LocationId { get; set; }
     [ForeignKey(nameof(FileId))]
     public File? File { get; set; }
     public bool ConnectAll { get; set; }
-    [ForeignKey(nameof(LocationId))]
-    public CrServerProxyLocation Location { get; set; }
-
+    public List<CrServerProxyLocation> Locations { get; set; }
     public List<ConfigurationRepositoryMiddlewareArgument> Arguments { get; set; } = [];
 }
