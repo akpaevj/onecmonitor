82096b8129226c8713dce75c8031be6c6e8cae14
Акпаев Евгений Александрович <akpaev@unichimtek.ru>
2026-01-30 09:07:16 +0300

    Удален тестовый проект

--- test/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using OneSwiss.V8.Updater;
-
-var info = await UpdateApi.GetConfigurationbAndPlatformUpdateInfo("Enterprise20", "2.5.22.134", "8.3.27.1786");
-
-var configUpdate = info!.AdditionalReleaseUpdate?.Count > 0 ? info.AdditionalReleaseUpdate.First().ConfigurationUpdate : info.ConfigurationUpdateResponse;
-
-var update = await UpdateApi.GetConfigurationbAndPlatformUpdate("", "", configUpdate!, info.PlatformUpdateResponse);
-var a = 1;
